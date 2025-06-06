086bfa2208f47d7a34226cb6b869cc6539a33a46
IbrahimHafez1 <ibrahimhafez1@yahoo.com>
2025-06-06 05:05:13 +0300

    revert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,6 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/slack/save-token': {
-        target: 'https://5d0c739d-620b-4833-b270-6da5909ef27a-00-38en1hwbeb1oi.kirk.replit.dev',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/slack\/save-token/, '/api/slack/save-token'), // Rewrites the path
-      },
       '/api': {
         target: 'https://5d0c739d-620b-4833-b270-6da5909ef27a-00-38en1hwbeb1oi.kirk.replit.dev',
         changeOrigin: true,
