7e9ea67db793f2109fa86a25bdb4210707592c15
IbrahimHafez1 <ibrahimhafez1@yahoo.com>
2025-06-07 01:09:03 +0300

    server static files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -37,6 +37,7 @@ dotenv.config();
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const frontendPath = path.resolve(__dirname, '../../frontend/dist');
 
+  app.use(express.static(path.join(__dirname, 'dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
