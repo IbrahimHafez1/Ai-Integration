dc3deefd9f05f2fb0606037b0c54b1ce5044445a
IbrahimHafez1 <ibrahimhafez1@yahoo.com>
2025-06-08 14:37:15 +0300

    fix user

--- frontend/src/pages/OAuth.jsx
+++ frontend/src/pages/OAuth.jsx
@@ -41,7 +41,7 @@ export default function IntegrationsPage() {
 
     switch (provider) {
       case 'slack':
-        redirectToSlackOAuth(user);
+        redirectToSlackOAuth(user._id);
         break;
       case 'google':
         redirectToGoogle();
