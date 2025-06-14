06dc45383455d9477783befa94f6a37d3a46cc35
IbrahimHafez1 <ibrahimhafez1@yahoo.com>
2025-06-14 22:17:05 +0300

    fix: Update build and start scripts in package.json; adjust TypeScript compiler options

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,3 +1,2 @@
-export { default as config } from './config.js';
-export { config as default } from './config.js';
+export { default } from './config.js';
 export type { Config } from './config.js';
