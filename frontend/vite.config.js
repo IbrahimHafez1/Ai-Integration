1bed1be2e812fefb11878d918c35583f19e2e198
IbrahimHafez1 <ibrahimhafez1@yahoo.com>
2025-06-14 03:27:02 +0300

    fix urls

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,35 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true, // Listen on all addresses
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:4000',
+        target: 'https://ai-integration-evmk.vercel.app',
         changeOrigin: true,
         secure: false,
-        ws: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response:', proxyRes.statusCode, req.url);
-          });
-        }
       },
       '/socket.io': {
-        target: 'http://127.0.0.1:4000',
-        changeOrigin: true,
-        ws: true
-      }
-    }
-  }
+        target: 'https://ai-integration-evmk.vercel.app',
+        ws: true,
+      },
+    },
+  },
 });
